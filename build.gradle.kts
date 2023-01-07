0e4c3c697d5ac463e325aae3ed09ef657c8cd846
Ilker Aslan <ilkerozguraslan@gmail.com>
2023-01-07 14:29:14 +0100

    Use plugins block instead of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,12 @@ buildscript {
 }
 
 plugins {
-//    id(libs.plugins.ktor.get().pluginId) version "2.2.2" apply false
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.android.application) apply false
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.android.library) apply false
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlin.serialization) apply false
-
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlin.android) apply false
 }
