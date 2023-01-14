1d0d4d96a43ee7be13bb6ba88caa5aaf8f0537a6
Ilker Aslan <ilkerozguraslan@gmail.com>
2023-01-14 13:40:44 +0100

    Remove useless targets from network

--- shared/network/build.gradle.kts
+++ shared/network/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("multiplatform")
-    id("com.android.library")
     kotlin("plugin.serialization")
 }
 
@@ -9,56 +8,7 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
-android {
-    compileSdk = 31
-    namespace = "me.ilker.shared.network"
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 31
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-
-        getByName("debug") {
-            isMinifyEnabled = false
-            isJniDebuggable = true
-        }
-    }
-}
-
 kotlin {
-    android()
-
-    iosArm64 {
-        /* Apple iOS on ARM64 platforms (Apple iPhone 5s and newer) */
-        this.binaries.framework {
-            baseName = "YetAnotherKMPNetwork"
-        }
-    }
-
-    iosX64 {
-        /* Apple iOS simulator on x86_64 platforms */
-        this.binaries.framework {
-            baseName = "YetAnotherKMPNetwork"
-        }
-    }
-
-    iosSimulatorArm64 {
-        /* Apple iOS simulator on Apple Silicon platforms */
-        this.binaries.framework {
-            baseName = "YetAnotherKMPNetwork"
-        }
-    }
-
     jvm()
 
     sourceSets {
@@ -72,27 +22,6 @@ kotlin {
             }
         }
 
-        val androidMain by sourceSets.getting {
-            dependencies {
-                implementation(libs.ktor.client.android)
-            }
-        }
-
         val jvmMain by sourceSets.getting
-
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-
-        val iosMain by sourceSets.creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-
-            dependencies {
-                implementation(libs.ktor.client.darwin)
-            }
-        }
     }
 }
